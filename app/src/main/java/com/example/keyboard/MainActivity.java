4f913bb7ddae2db717b6b8130949a3f79b9af5cb
NguyenVanLam260398 <lamnv260398@gmail.com>
2021-07-21 20:27:04 +0700

    Add text show content view

--- app/src/main/java/com/example/keyboard/MainActivity.java
+++ app/src/main/java/com/example/keyboard/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.keyboard;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        int uiOption = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOption);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
